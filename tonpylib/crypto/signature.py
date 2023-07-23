ca1f69572857a18f841d3aa0ac928011492ff0a7
yungwine <cyrbatoff@gmail.com>
2023-07-23 18:23:49 +0800

    add keys to crypto

--- tonpylib/crypto/signature.py
+++ tonpylib/crypto/signature.py
@@ -1,4 +1,7 @@
-from nacl.signing import VerifyKey, exc
+from nacl.signing import VerifyKey, exc, SignedMessage
+from nacl.bindings import crypto_sign, crypto_sign_BYTES
+import nacl.encoding
+
 
 
 def verify_sign(public_key: bytes, signed_message: bytes, signature: bytes):
@@ -8,3 +11,15 @@ def verify_sign(public_key: bytes, signed_message: bytes, signature: bytes):
         return True
     except exc.BadSignatureError:
         return False
+
+
+def sign_message(message: bytes,
+                 signing_key,
+                 encoder: nacl.encoding.Encoder = nacl.encoding.RawEncoder, ) -> bytes:
+    raw_signed = crypto_sign(message, signing_key)
+
+    signature = encoder.encode(raw_signed[:crypto_sign_BYTES])
+    message = encoder.encode(raw_signed[crypto_sign_BYTES:])
+    signed = encoder.encode(raw_signed)
+
+    return SignedMessage._from_parts(signature, message, signed).signature
