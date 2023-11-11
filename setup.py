e52c7b2b1dd36aaac2e4f7d34efa3acc9a38d4e8
Maksim Kurbatov <94808996+yungwine@users.noreply.github.com>
2023-11-12 00:36:29 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pytoniq",
-    version="0.1.13",
+    version="0.1.14",
     author="Maksim Kurbatov",
     author_email="cyrbatoff@gmail.com",
     description="TON Blockchain SDK",
