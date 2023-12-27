8a5929d10e05d6c0c8c84a2325479e5e15481f7b
Maksim Kurbatov <94808996+yungwine@users.noreply.github.com>
2023-12-27 13:48:52 +0700

    fix highload import

--- pytoniq/contract/wallets/__init__.py
+++ pytoniq/contract/wallets/__init__.py
@@ -1,1 +1,2 @@
-from .wallet import WalletError, Wallet, BaseWallet, WalletV3, WalletV4, WalletV3R1, WalletV3R2, WalletV4R2, WalletV3Data, WalletV4Data, WalletMessage, HighloadWallet
+from .wallet import WalletError, Wallet, BaseWallet, WalletV3, WalletV4, WalletV3R1, WalletV3R2, WalletV4R2, WalletV3Data, WalletV4Data, WalletMessage
+from .highload import HighloadWallet
