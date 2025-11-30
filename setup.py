135fdbd01177e247d7a78d346d3777745ab9ab9d
yungwine <cyrbatoff@gmail.com>
2025-11-30 20:28:57 +0800

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pytoniq",
-    version="0.1.42",
+    version="0.1.43",
     author="Maksim Kurbatov",
     author_email="cyrbatoff@gmail.com",
     description="TON Blockchain SDK",
